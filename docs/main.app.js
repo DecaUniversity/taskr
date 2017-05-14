b0412fd9f447ef9a348c94747c8ac51ff6b12fd6
DecaHub <dan@decahub.com>
2017-05-14 11:06:35 -0400

    Build for prod.

---
+++ docs/main.app.js
@@ -0,0 +1,4 @@
+"use strict";
+
+angular.module("main", ["ui.router"]);
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uYXBwLmpzIl0sIm5hbWVzIjpbImFuZ3VsYXIiLCJtb2R1bGUiXSwibWFwcGluZ3MiOiJBQUFBOztBQUVBQSxRQUFRQyxNQUFSLENBQWUsTUFBZixFQUF1QixDQUFDLFdBQUQsQ0FBdkIiLCJmaWxlIjoibWFpbi5hcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcblxuYW5ndWxhci5tb2R1bGUoXCJtYWluXCIsIFtcInVpLnJvdXRlclwiXSk7XG5cblxuIl19
