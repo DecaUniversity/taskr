2f2280d2db87dadafdc9faea30a934ac60f0e52b
DecaHub <dan@decahub.com>
2017-05-14 10:56:16 -0400

    Create ProgressDisk Directive + update eslint to accommodate for jQuery

--- app/components/progressDisk/progressDisk.directive.js
+++ app/components/progressDisk/progressDisk.directive.js
@@ -1,46 +1,82 @@
 "use strict";
 
-/**
+/* eslint-disable  no-ternary */
+/* eslint-disable  quotes */
+ 
+ /**
  * @desc progress disk directive that shows how many tasks have been
  * completed.
  * @example <dh-progress-disk></dh-progress-disk>
  */
 
-angular
-	.module("main")
-	.directive("dhProgressDisk", dhProgressDisk);
-
-function dhProgressDisk() {
+angular.module("main").directive("dhProgressDisk", ["ProgressService", function (ProgressService) {
 	
-	let directive = {
+	return {
 		
-		link: link,
-		restrict: "E",
-		templateUrl: "components/progressDisk/progressDisk.html"
+		templateUrl: "components/progressDisk/progressDisk.html",
+		scope: {
+			size: "<",
+			thickness: "<",
+			fill: "<",
+			cycle: "<"
+		},
+		link (scope, element, attributes) {
+			
+			const radiansMultiplier = 2.5;
+			
+			const circle = $("#disk", element);
+			const progressText = $("#progress", element);
+			const startAngle = Math.PI * radiansMultiplier * -1;
+			
+			const minSize = 150;
+			const minThickness = 3;
+			const defaultThickness = 10;
+			const defaultFill = "white";
+			
+			const fullProgress = 100;
+			
+			progressText.css("color", scope.fill);
+			
+			scope.$watch(ProgressService.getProgress, function (newVal, oldVal) {
+				
+				circle.circleProgress({
+					value: newVal / scope.cycle,
+					size:
+						scope.size >= minSize
+							? scope.size
+							: minSize,
+					startAngle,
+					thickness:
+						scope.thickness <= scope.size / 2 && scope.thickness >= minThickness
+							? scope.thickness
+							: defaultThickness,
+					fill:
+						scope.fill
+							? scope.fill
+							: defaultFill
+					
+				}).on("circle-animation-progress", function (event, progress, stepValue) {
+					
+					const displayStepValue = (stepValue * fullProgress).toFixed(0);
+					
+					if (displayStepValue == fullProgress) { // eslint-disable-line eqeqeq
+						
+						progressText.text("");
+						progressText.css("font-size", "500%");
+						progressText.append('<i class="fa fa-trophy" aria-hidden="true"></i>');
+						
+						return;
+						
+					}
+					
+					progressText.text(displayStepValue);
+					
+				});
+				
+			});
+			
+		}
 		
 	};
 	
-	function link (scope, elem, attr) {
-		
-		let taskCount = 5;
-		
-		$('#disk', elem).circleProgress({
-			value: (1 / 6) * taskCount,
-			size: 180,
-			startAngle: -Math.PI * 0.5,
-			thickness: 10,
-			fill: {
-				gradient: ["white", "white"]
-			}
-		}).on("circle-animation-progress", function (event, progress, stepValue) {
-			
-			let displayStepValue = (stepValue * 100).toFixed(0);
-			
-			$("#progress", elem).text(displayStepValue);
-		});
-		
-	}
-	
-	return directive;
-	
-}
+}]);
